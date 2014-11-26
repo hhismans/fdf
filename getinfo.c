0ca8848259102946bffaa96d6922937cfa022f25
hhismans <hhismans@student.42.fr>
2014-11-27 00:59:01 +0100

    ok

--- getinfo.c
+++ getinfo.c
@@ -6,13 +6,14 @@
 /*   By: hhismans <hhismans@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/24 11:58:03 by hhismans          #+#    #+#             */
-/*   Updated: 2014/11/26 23:41:55 by hhismans         ###   ########.fr       */
+/*   Updated: 2014/11/27 00:58:44 by hhismans         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <fcntl.h>
 #include <unistd.h>
+#include "fdf.h"
 
 static int		*filltab(char **tab_char)
 {
@@ -50,55 +51,64 @@ int		**getinfo(char *file)
 	fd = open(file, O_RDONLY);
 	if ((ret = get_next_line(fd, &line)))
 	{
-		ft_putstr("contenu de line:");
-		ft_putendl(line);
 		lst_line = ft_lstnew(line, sizeof(char) * (ft_strlen(line) + 1));
-		ft_putstr("contenu de lst_line->content:");
-		ft_putendl(lst_line->content);
 		nbr_line++;
 	}
 	tmp = lst_line;
 	while ((ret = get_next_line(fd, &line)))
 	{
-		ft_putstr("contenu de line ; ");
-		ft_putendl(line);
 		tmp->next = ft_lstnew(line, sizeof(char) * (ft_strlen(line) + 1));
 		tmp = tmp->next;
-		ft_putstr("contenu de tmp->content ; ");
-		ft_putendl(tmp->content);
 		nbr_line++;
 	}
 	close(fd);
 	i = 0;
 	tab_int = (int **)ft_taballoc(sizeof(int) * (nbr_line));
 	tmp = lst_line;
-	ft_putstr("contenu de tmp->content  @; ");
-	ft_putendl(tmp->next->content);
 	while (tmp)
 	{
-		ft_putstr("tmp content debut : ");
-		ft_putendl(tmp->content);
 		tab_char = ft_strsplit((char *)tmp->content, ' ');
-		ft_putendl("ANALYSE TABCHAR");
-			int t = 0;
-			while (tab_char[t])
-			{
-			ft_putstr("elem :");
-			ft_putendl(tab_char[t]);
-		t++;
-		}
 		tab_int[i] = filltab(tab_char);
 		i++;
 		tmp = tmp->next;
-		//ft_putendl(tmp->content);
 	}
-	//ft_lstdel(&lst_line);
 	return(tab_int);
+}
+
+int		**convert_cavaliere(int **tab_int)
+{
+	int **tab_cav;
+	int size;
+	int i;
+	int j;
+	//t_point
+	size = 0;
+	while (tab_int[size])
+		size++;
+	i = 0;
+	j = 0;
+	tab_cav = (int **)ft_taballoc(sizeof(int) * (size + 1));
+	while (tab_int[j])
+	{
+			ft_putendl("while EXTERNE");
+		tab_cav[j] = (int *)ft_memalloc(tab_int[j][0] + 1);
+		tab_cav[j][0] = tab_int[j][0];
+		i = 1;
+		while (i <= tab_int[j][0])
+		{
+			//ft_putendl("while interne");
+			tab_cav[j][i] = (i - 1) + (35 * tab_int[j][i]);
+			i++;
+		}
+		j++;
+	}
+	return (tab_cav);
 }
 
 int		main(int argc, char **argv)
 {
 	int **tab;
+	int **tab_cav;
 	int i;
 	int j;
 
@@ -111,7 +121,24 @@ int		main(int argc, char **argv)
 		while (i <= tab[j][0])
 		{
 			ft_putnbr(tab[j][i]);
-			ft_putstr(" ");
+			ft_putstr("\t");
+			i++;
+		}
+		ft_putendl("");
+		j++;
+	}
+	i = 0;
+	j = 0;
+	ft_putendl("CONVERTI");
+	tab_cav = convert_cavaliere(tab);
+	ft_putendl("CONVERTI");
+	while (tab_cav[j])
+	{
+		i = 0;
+		while (i <= tab_cav[j][0])
+		{
+			ft_putnbr(tab_cav[j][i]);
+			ft_putstr("\t");
 			i++;
 		}
 		ft_putendl("");
